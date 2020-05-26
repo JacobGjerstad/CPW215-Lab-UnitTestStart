81fb7b9672d72d69b694e7f83a715a988e11d08e
JacobGjerstad <56143070+JacobGjerstad@users.noreply.github.com>
2020-05-26 14:40:42 -0700

    Completed course class (#3)

--- BusinessLogic/Course.cs
+++ BusinessLogic/Course.cs
@@ -6,9 +6,14 @@ namespace BusinessLogic
 {
     public class Course
     {
+        private string courseName;
+        private byte numCredits;
+        private byte maxCredits = 30;
+
         public Course(string courseName)
         {
             CourseName = courseName;
+            InstructorName = "STAFF";
         }
 
         /// <summary>
@@ -19,12 +24,36 @@ namespace BusinessLogic
         /// <summary>
         /// The name of the course
         /// </summary>
-        public string CourseName { get; set; }
+        public string CourseName 
+        { 
+            get => courseName;
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentException("Must have a course name.");
+                }
+
+                courseName = value;
+            } 
+        }
 
         /// <summary>
         /// Number of credits for the course
         /// </summary>
-        public byte NumberOfCredits { get; set; }
+        public byte NumberOfCredits 
+        {
+            get => numCredits;
+            set
+            {
+                if(value >= maxCredits || value <= 0)
+                {
+                    throw new ArgumentException("Invalid number of credits.");
+                }
+
+                numCredits = value;
+            } 
+        }
 
     }
 }
